7a06269a0d63e6050bb332b93cfedbbee99c72f9
ahmednour1234 <ahmednour5999@gmail.com>
2025-12-29 17:22:06 +0200

    wqwqe

--- resources/views/filament/components/topbar-right-cluster.blade.php
+++ resources/views/filament/components/topbar-right-cluster.blade.php
@@ -1,4 +1,4 @@
-<div class="fi-topbar-right-cluster flex items-center gap-3">
+<div class="fi-topbar-right-cluster flex items-center gap-x-4 px-2">
     @livewire(\App\Filament\Components\LanguageSwitcherTopbar::class, key('language-switcher-topbar'))
     @livewire(\App\Filament\Components\NotificationsDropdown::class, key('notifications-dropdown-topbar'))
 </div>
