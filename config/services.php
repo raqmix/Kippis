7d9017304c43986c5d1b5a05ce97a6dfc04be882
jamalahmadd <jamal@verozonesolutions.com>
2026-02-03 23:15:21 +0200

    Add Apple Sign In configuration to .env.example and services.php

--- config/services.php
+++ config/services.php
@@ -43,6 +43,7 @@ return [
 
     'apple' => [
         'client_id' => env('APPLE_CLIENT_ID'),
+        'client_secret' => env('APPLE_CLIENT_SECRET'),
         'team_id' => env('APPLE_TEAM_ID'),
         'key_id' => env('APPLE_KEY_ID'),
         'private_key' => env('APPLE_PRIVATE_KEY'),
