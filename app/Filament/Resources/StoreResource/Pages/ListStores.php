aa083962a10c34122abc33e7305a08c0938ba8d7
jamalahmadd <jamal@verozonesolutions.com>
2026-04-05 18:38:14 +0200

    feat: Add syncBranches action to ListStores; implement branch synchronization and notifications

--- app/Filament/Resources/StoreResource/Pages/ListStores.php
+++ app/Filament/Resources/StoreResource/Pages/ListStores.php
@@ -3,7 +3,9 @@
 namespace App\Filament\Resources\StoreResource\Pages;
 
 use App\Filament\Resources\StoreResource;
+use App\Modules\Stores\Services\FoodicsBranchesSyncService;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ListRecords;
 use Illuminate\Database\Eloquent\Builder;
 
@@ -14,6 +16,32 @@ class ListStores extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
+            Actions\Action::make('syncBranches')
+                ->label(__('system.sync_branches'))
+                ->icon('heroicon-o-arrow-path')
+                ->color('success')
+                ->action(function () {
+                    try {
+                        $syncService = app(FoodicsBranchesSyncService::class);
+                        $stats = $syncService->syncAllBranches();
+
+                        Notification::make()
+                            ->title(__('system.sync_branches_success'))
+                            ->body(__('system.sync_branches_success_body', [
+                                'created' => $stats['created'],
+                                'updated' => $stats['updated'],
+                                'skipped' => $stats['skipped'],
+                            ]))
+                            ->success()
+                            ->send();
+                    } catch (\Exception $e) {
+                        Notification::make()
+                            ->title(__('system.sync_branches_failed'))
+                            ->body($e->getMessage())
+                            ->danger()
+                            ->send();
+                    }
+                }),
             Actions\CreateAction::make(),
         ];
     }
